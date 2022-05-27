658eabc572c5dbb91dcf4aa174dc5300f9d70a04
Aman Ullah <84645417+Amanullahkhankingas@users.noreply.github.com>
2022-05-27 21:38:35 +0500

    Updates

---
+++ precache-manifest.41050bfa109bad25bc9e5f8a41fb3ef3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1941bb087ee8eef7f0fc",
+    "url": "/TextUtils%20-Home/static/css/main.9296f441.chunk.css"
+  },
+  {
+    "revision": "1941bb087ee8eef7f0fc",
+    "url": "/TextUtils%20-Home/static/js/main.1941bb08.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/TextUtils%20-Home/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/TextUtils%20-Home/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "f089076bef512f3246c6",
+    "url": "/TextUtils%20-Home/static/js/runtime~main.f089076b.js"
+  },
+  {
+    "revision": "70d32076ec3672ffe6d1543da61e0f56",
+    "url": "/TextUtils%20-Home/index.html"
+  }
+];
